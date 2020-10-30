5363638291a0215be35f84837a6f773a4b3d2289
Elvis Nunez <elvisnunez@me.com>
2020-10-30 14:58:40 +0100

    Add menu principal

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -19,5 +19,43 @@ class MainMenuWidget extends StatelessWidget {
 
 // Reemplaza con tu codigo
 Widget _myView(BuildContext context) {
-  return ListView();
+  return Center(
+    child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Text("Bienvenido(a): Elvis Nunez")
+      ),
+      Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: FittedBox(
+            fit: BoxFit.fill, // otherwise the logo will be tiny
+            child: const FlutterLogo(),
+          ),
+      ),
+      ElevatedButton(
+        child: Text('Menu Semanal'),
+        onPressed: () {
+          // Navigate to second route when tapped.
+        },
+      ),
+      ElevatedButton(
+        child: Text('Platos a la Carta'),
+        onPressed: () {
+          // Navigate to second route when tapped.
+        },
+      ),
+      ElevatedButton(
+        child: Text('Historial de Compras'),
+        onPressed: () {
+          // Navigate to second route when tapped.
+        },
+      ),
+      ElevatedButton(
+        child: Text('Ayudanos a mejorar'),
+        onPressed: () {
+          // Navigate to second route when tapped.
+        },
+      ),
+    ]),
+  );
 }
